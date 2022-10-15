318f5f71bd047c789f07af62690f0f322716de13
efee <efee@cs.sfu.ca>
2022-10-15 10:17:46 -0700

    init commit

---
+++ app/src/main/java/com/crunglers/speedcheckers/Piece.java
@@ -0,0 +1,4 @@
+package com.crunglers.speedcheckers;
+
+public class Piece {
+}
---
+++ app/src/main/java/com/crunglers/speedcheckers/startActivity.java
@@ -0,0 +1,14 @@
+package com.crunglers.speedcheckers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class startActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
